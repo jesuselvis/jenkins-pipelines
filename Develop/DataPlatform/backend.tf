710817cb0fd83c4f7a3eb64f65c68f90d32e9ff7
frontend-at-683418597746 <vcaxi@cajalosandes.pe>
2024-05-02 18:04:28 -0500

    v0.00.6

--- Develop/DataPlatform/backend.tf
+++ Develop/DataPlatform/backend.tf
@@ -3,10 +3,10 @@ terraform{
         bucket = "devops-iac-dev-jenkins-tfstate"
         encrypt = true
         key = "dataplatform/terraform.tfstate"
-        region = "us-east-1"
+        # region = "us-east-1"
     }
 }
 
-provider "aws" {
-  region  = "us-east-1"
-}
+# provider "aws" {
+#   region  = "us-east-1"
+# }
