072b490950e9fe94ec2e6fc4f9851935d9ac4bdd
frontend-at-683418597746 <vcaxi@cajalosandes.pe>
2024-05-07 19:52:44 -0500

    v0.0.40

--- Develop/DataPlatform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform{
-    backend "s3" {
-        bucket = "devops-iac-dev-jenkins-tfstate"
-        encrypt = true
-        key = "dataplatform/terraform.tfstate"
-        region = "us-east-1"
-    }
-}
